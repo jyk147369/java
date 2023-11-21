7fa6c42f173a615f4476576e3362e4ff20ad8955
jyk147369 <jyk147369@gmail.com>
2023-11-21 15:08:25 +0900

    [Docs] day02 java practice

--- src/day02/Ex07_2.java
+++ src/day02/Ex07_2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Ex07_2 {
     public static void main(String[] args) {
-        int [] unit = {50000, 10000, 1000, 500, 100, 50, 10, 1};
+        int [] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
         // 정수로 된 돈의 액수를 입력 받기
         // ex)675652원
@@ -16,11 +16,11 @@ public class Ex07_2 {
 
         System.out.println("입력하신 돈은 " + money + "원 입니다.");
 
-        int[] ea = new int[8];
+        int[] ea = new int[9];
 
-        for(int i=0; i<=7; i++){
+        for(int i=0; i<9; i++){
             ea[i] = money/unit[i];
-            System.out.println(unit[i]+"원 개수는 " + ea[i] + "장 입니다.");
+            System.out.println(unit[i]+"원 개수는 " + ea[i] + "개 입니다.");
             money = money - ea[i]*unit[i];
         }
     }
