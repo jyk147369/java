cc0f6607139e935b75e7a2f9612c3e50b1d2e8f3
jyk147369 <jyk147369@gmail.com>
2023-11-24 01:45:22 +0900

    [Docs] day04 java practice

--- src/day04/CarRacingPlay.java
+++ src/day04/CarRacingPlay.java
@@ -32,14 +32,18 @@ public class CarRacingPlay {
             car[i] = new Car(carName[i], 0);
         }
 
+        // 경주 횟수 입력
         Scanner sc3 = new Scanner(System.in);
         System.out.println("경주 횟수를 입력하세요 : ");
         Integer raceNum = sc3.nextInt();
 
+
+        // 경주 횟수만큼 세트 반복
         for (int j=0; j<raceNum; j++) {
 
             System.out.println("["+(j+1)+"회차]");
 
+            // 자동차 개수만큼 랜덤뽑기 반복
             for (int i = 0; i < carNum; i++) {
                 car[i].moving();
             }
@@ -49,17 +53,19 @@ public class CarRacingPlay {
             car[i].finalGoal();
         }
 
+        // 일단 배열 중 첫번째를 최대값 변수에 저장
         Integer maxCarX=car[0].carX;
         for (int i=0; i<carNum; i++){
+            // 배열 중 첫번째와 나머지들을 비교하면서 큰 수가 있으면 최대값 변수에 저장하도록
             if(car[0].carX<=car[i].carX){
-                maxCarX = car[i].carX;
+                maxCarX = car[i].carX;      // 결국 최대값을 가진 car[i].carX가 최대값 변수에 저장됨
             }
         }
 
         System.out.print("최종 우승자 : ");
         for(int i=0; i<carNum; i++) {
             if (maxCarX == car[i].carX) {
-                System.out.print(car[i].carName+" ");
+                System.out.print(car[i].carName + " ");
             }
         }
 
